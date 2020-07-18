beda0283f8d92119c30491b873ee7e3a1c7fdc8f
NullVoxPopuli <LPSego3+dev@gmail.com>
2020-07-18 14:47:10 -0400

    shoelace 2 broken. lol

--- client/web/addons/ui/ember-cli-build.js
+++ client/web/addons/ui/ember-cli-build.js
@@ -1,10 +1,20 @@
 'use strict';
 
+const path = require('path');
+
 // eslint-disable-next-line node/no-unpublished-require
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
+const shoelace = path.dirname(require.resolve('@shoelace-style/shoelace'));
+
 module.exports = function (defaults) {
-  let app = new EmberAddon(defaults, {});
+  let app = new EmberAddon(defaults, {
+    autoImport: {
+      alias: {
+        '@shoelace-style/shoelace': `@shoelace-style/shoelace/dist/collection/index.js`,
+      },
+    },
+  });
 
   /*
     This build file specifies the options for the dummy test app of this
@@ -12,6 +22,9 @@ module.exports = function (defaults) {
     This build file does *not* influence how the addon or the app using it
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
+
+  // app.import(`${shoelace}/shoelace/shoelace.esm.js`);
+  // app.import(`${shoelace}/shoelace/shoelace.css`);
 
   return app.toTree();
 };
